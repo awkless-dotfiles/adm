8c65caf3136ab353cb60bcc2b4f9cd0b2653f1aa
Jason Pena <jasonpena@awkless.com>
2024-03-12 23:26:12 -0600

    feat(main): Setup basic main()
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+/// Start of execution.
+///
+/// # Preconditions
+///
+/// None.
+///
+/// # Postconditions
+///
+/// None.
+///
+/// # Since
+///
+/// 0.1.0
+fn main() {
+    println!("Hello there!");
+}
