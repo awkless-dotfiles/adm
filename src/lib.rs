3320bd941512ef5174b55aedf978acbebc1f7bef
Jason Pena <jasonpena@awkless.com>
2024-03-14 19:41:41 -0600

    feat(lib.rs): Include `adm::error` and `adm::vcs` as modules
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+/// The `adm::error` module containing all possible error types in `adm` binary.
+pub mod error;
+
+/// The `adm::vcs` module contains API's for manipulating command-line VCS tools.
+pub mod vcs;
