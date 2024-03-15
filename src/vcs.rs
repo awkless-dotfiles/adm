feb02c101c6823d88dbfdf00a685fcb74db80a33
Jason Pena <jasonpena@awkless.com>
2024-03-14 19:40:19 -0600

    feat(adm::vcs): Test execute() function
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/vcs.rs
+++ src/vcs.rs
@@ -4,6 +4,7 @@
 use crate::error::VcsError;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use log::debug;
 
 /// Run a shell command from user's system.
 ///
@@ -18,7 +19,7 @@ use std::process::Command;
 ///
 /// # Arguments
 ///
-/// * `name`[in] - Name of shell command to execute.
+/// * `cmd`[in] - Name of shell command to execute.
 /// * `args`[in] - Arguments to pass to shell command.
 ///
 /// # Returns
@@ -31,6 +32,7 @@ use std::process::Command;
 /// 0.2.0
 fn execute(cmd: &str, args: &[&str]) -> Result<String, VcsError> {
     let fullcmd: String = format!("{} {}", cmd, args.join(" ")).into();
+    debug!("Execute {}", fullcmd);
 
     let output = Command::new(cmd)
         .args(args)
@@ -48,4 +50,40 @@ fn execute(cmd: &str, args: &[&str]) -> Result<String, VcsError> {
         .map_err(|error| VcsError::ShellCmdUtf8Error { cmd: fullcmd.clone(), errsrc: error })?;
 
     Ok(outmsg)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Test execute() function.
+    ///
+    /// # Expectations
+    ///
+    /// 1. Get VcsError::ShellCmdSpawnError for invalid command.
+    /// 2. Get VcsError::ShellCmdUtf8Error for invalid UTF-8 in stderr.
+    /// 3. Get VcsError::ShellCmdUtf8Error for invalid UTF-8 in stdout.
+    /// 4. Get VcsError::ShellCmdError for command exiting non-successfully.
+    /// 5. Execute shell command without failure.
+    ///
+    /// # Since
+    ///
+    /// 0.2.0
+    #[test]
+    fn test_execute() {
+        let badcmd = execute("deadbeef", &["--bad"]);
+        assert!(matches!(badcmd, Err(VcsError::ShellCmdSpawnError { .. })));
+
+        let mut badutf8 = execute("sh", &["-c", "printf \"\\x0\\x9F\" >&2; exit 1"]);
+        assert!(matches!(badutf8, Err(VcsError::ShellCmdUtf8Error { .. })));
+
+        badutf8 = execute("sh", &["-c", "printf \"\\x0\\x9F\"; exit 0"]);
+        assert!(matches!(badutf8, Err(VcsError::ShellCmdUtf8Error { .. })));
+
+        let cmdfail = execute("ls", &["--bad-option"]);
+        assert!(matches!(cmdfail, Err(VcsError::ShellCmdError { .. })));
+
+        let goodcmd = execute("echo", &["hello world"]);
+        assert_eq!("hello world\n", goodcmd.unwrap());
+    }
 }
